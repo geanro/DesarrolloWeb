c302c372d2f56b5cb4a29f56bf3d8b392be4b45f
geanro <geanrojasa21@gmail.com>
2026-04-22 16:50:05 -0500

    first commit

---
+++ demo/src/main/java/com/example/demo/model/Producto.java
@@ -0,0 +1,61 @@
+package com.example.demo.model;
+
+public class Producto {
+
+    private int id;
+    private String nombre;
+    private double precio;
+    private int stock;
+    private String categoria;
+    
+    public Producto() {
+    }
+
+    public Producto(int id, String nombre, double precio, int stock, String categoria) {
+        this.id = id;
+        this.nombre = nombre;
+        this.precio = precio;
+        this.stock = stock;
+        this.categoria = categoria;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+}
